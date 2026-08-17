d10ce3af98fe2dda6360ae37642dcee9aa3d247d
João Vitor Zanferrari Morais <joaovzanferrarim@gmail.com>
2026-08-17 16:24:44 -0300

    Método quase finalizado

--- exercicio3/Estudante.java
+++ exercicio3/Estudante.java
@@ -40,5 +40,24 @@ public class Estudante {
     public double[] getNotas() {
         return notas;
     }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+        public static Estudante[] aprovados(Estudante[] estudantes) {
+        int quantidadeAprovados = 0;
+       
+        for(int i =0; i<estudantes.length; i++){
+            if(estudantes[i].calculaMedia() >= 6){
+                quantidadeAprovados ++;
+            }
+        }
+        if(quantidadeAprovados ==0){
+            return null;
+        }
+        Estudante[] aprovados = new Estudante[quantidadeAprovados];
+
+        int posicao =0;
+  for(int i=0; i<estudantes.length; i++){
+    if(estudantes[i].calculaMedia() >=6){
+        aprovados[posicao] = estudantes[i];
+        posicao ++;
+    }
+  }
 }
