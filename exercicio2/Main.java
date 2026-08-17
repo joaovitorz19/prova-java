5e3aeb2a6095bbd1c7cdb3f6abc1064915de3322
João Vitor Zanferrari Morais <joaovzanferrarim@gmail.com>
2026-08-17 14:53:14 -0300

    inicio do main,estudante1 e estudante2

--- exercicio2/Main.java
+++ exercicio2/Main.java
@@ -1,5 +1,15 @@
 package exercicio2;
 
 public class Main {
-    
+     public static void main (String[]args){
+        Estudante estudante1 = new Estudante("João");
+        Estudante estudante2 =new estudante("Maria");
+     }
+     System.out.println("Digite as notas de ") + estudante1.getNome());
+     estudante1.insereNotas();
+
+     System.out.println("\nDigite as notas de " + estudante2.getNome());
+     estudante2.insereNotas();
+
+     System.out.println("\n--- RESULTADOS ---");
 }
