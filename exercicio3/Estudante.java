a3688815a4fb748c3f0edfce59998795c15030a5
João Vitor Zanferrari Morais <joaovzanferrarim@gmail.com>
2026-08-17 16:11:06 -0300

    classe estuadante fieta nates de coloclocar o método

--- exercicio3/Estudante.java
+++ exercicio3/Estudante.java
@@ -1,6 +1,44 @@
 package exercicio3;
 
+import java.util.Scanner;
+
 public class Estudante {
     private String nome;
     private double[] notas;
+  public Estudante(String nome) {
+        this.nome = nome;
+        this.notas = new double[5];
+    }
+
+    
+    public void insereNotas() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Digite a nota " + (i + 1) + ": ");
+            notas[i] = scanner.nextDouble();
+        }
+    }
+
+   
+    public double calculaMedia() {
+
+        double soma = 0;
+
+        for (double nota : notas) {
+            soma += nota;
+        }
+
+        return soma / notas.length;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 }
