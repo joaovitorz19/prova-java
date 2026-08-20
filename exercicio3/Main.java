7bd151289bd1f903054112f70ccd824ba4655244
João Vitor <2024322021@aluno.ifrs.edu.br>
2026-08-19 22:58:54 -0300

    digitação das notas dos estudnates implementadas

--- exercicio3/Main.java
+++ exercicio3/Main.java
@@ -2,16 +2,17 @@ package exercicio3;
 
 public class Main {
         public static void main(String[] args){
-            Estudnate[] estudantes = new Estudante[3];
+            Estudante[] estudantes = new Estudante[3];
 
         
             estudantes[0] = new Estudante("João");
-            estudantes[1] = new Estudante('Maria');
+            estudantes[1] = new Estudante("Maria");
             estudantes[2] = new Estudante("Pedro");
 
-            for ( int i=0;i < estduantes.lenght; i++) {
-                System.out.println("")
+            for ( int i=0;i < estudantes.length; i++) {
+                System.out.println(" \n Notas de " + estudantes[i].getNome());
+                estudantes[i].insereNotas();
             }
-
+       
         }
 }
