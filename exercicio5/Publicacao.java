d3d88c856cc44ebd77190ce75ab1ec0b330255ce
João Vitor <2024322021@aluno.ifrs.edu.br>
2026-08-20 23:25:39 -0300

    publicacao feito

--- exercicio5/Publicacao.java
+++ exercicio5/Publicacao.java
@@ -1,5 +1,11 @@
 package exercicio5;
 
 public class Publicacao {
-    
+    private String titulo;
+    public Publicacao(String titulo){
+        this.titulo = titulo;
+    }
+    public String gettitulo() {
+        return titulo;
+    }
 }
