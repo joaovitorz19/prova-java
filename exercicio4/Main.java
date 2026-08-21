734be4986259985251132b8fbf3b0c26877db50e
João Vitor <2024322021@aluno.ifrs.edu.br>
2026-08-20 23:05:10 -0300

    calcula e  exibe a media ponderada

--- exercicio4/Main.java
+++ exercicio4/Main.java
@@ -3,9 +3,13 @@ package exercicio4;
 public class Main {
     public static void main(String[] args){
         Estudante estudante =new Estudante ("Joao");
+
         estudante.insereNotas();
+        
         System.out.println("Media aritmetica:" + estudante.calculaMedia());
         
         int[] pesos= {1,2,1,3,3};
+
+        System.out.println("Media ponderada:" + estudante.calculaMedia(pesos));
     }
 }
