db2564202c74a208af318e08529e5ad574ee2512
João Vitor <2024322021@aluno.ifrs.edu.br>
2026-08-20 23:30:49 -0300

    Construtor do livro e emprestimo feito

--- exercicio5/Livro.java
+++ exercicio5/Livro.java
@@ -1,5 +1,19 @@
 package exercicio5;
 
-public class Livro {
-    
+public class Livro extends Publicacao {
+    private boolean disponivel;
+
+    public Livro(String titulo){
+        super(titulo);
+        this.disponivel =true;
+    }
+    public void emprestar(){
+        if(disponivel) {
+            disponivel =false;
+            System.out.println("Livro emprestado com sucesso.");
+        }
+        else System.out.println("Livro já esta disnopivel.");
+    }
+    }
+
 }
