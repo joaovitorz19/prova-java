f54ecf3b3402fdd494cabc5ca16c04e3908a1ca2
João Vitor <2024322021@aluno.ifrs.edu.br>
2026-08-21 10:31:30 -0300

    Periodico feito

--- exercicio5/Periodico.java
+++ exercicio5/Periodico.java
@@ -1,5 +1,15 @@
 package exercicio5;
 
-public class Periodico {
-    
-}
+
+    public class Periodico extends Publicacao{
+         private int numeroVolume;
+
+         public Periodico(String titulo,int numerovolume){
+            super(titulo);
+            this.numeroVolume = numeroVolume;
+         }
+         public int getNumeroVolume(){
+            return numeroVolume;
+         }
+    }
+          
