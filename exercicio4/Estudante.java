35baaaf550109c4870f5c86c7362315bd6f0b5c5
João Vitor <2024322021@aluno.ifrs.edu.br>
2026-08-20 22:40:57 -0300

    Adicionado construtor do exercicio4

---
+++ exercicio4/Estudante.java
@@ -0,0 +1,15 @@
+package exercicio4;
+import java.util.Scanner;
+
+public class Estudante {
+    private String nome;
+    private double[] notas;
+
+    public Estudante(String nome) {
+        this.nome = nome;
+        this.notas= new double[5];
+    }
+    public void insereNotas() {
+        
+    }
+}
