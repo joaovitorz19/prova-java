cd92e8c3ee35eaf8da4783ae316b25b070da7530
João Vitor <2024322021@aluno.ifrs.edu.br>
2026-08-22 14:36:31 -0300

    funcao devolver terminado

--- exercicio5/Main.java
+++ exercicio5/Main.java
@@ -4,12 +4,17 @@ public class Main {
     public static void main(String[] args){
         Livro livro = new Livro("Dom Casmurro");
         Periodico periodico = new Periodico("Revista de tecnologia",15);
-        System.out.println("Livro:" + livro.getTitulo());
+        System.out.println("Livro:" + livro.gettitulo());
         System.out.println("Disponivel:" + livro.isDisponivel());
 
-        livro.empresta();
-        System.out.println("Livro:" + livro.getTitulo());
+        livro.emprestar();
+        System.out.println("Livro:" + livro.gettitulo());
         System.out.println("Disponivel:" + livro.isDisponivel());
+
+        livro.devolver();
+        System.out.println("Disponivel:" + livro.isDisponivel());
+        System.out.println("-----------");
         
+
     }
 }
